b6e57006d4de310a592dab112fe0c51b12b107b3
Alex Hutiev <abhutiev@gmail.com>
2021-03-26 00:44:23 +0300

    ex01 day00 done

--- day00/ex01/Program.java
+++ day00/ex01/Program.java
@@ -1,5 +1,33 @@
-public class Program {
-	public static void main(String[]) {
+import java.util.Scanner;
 
+public class Program {
+	private static long isSimple(long num) {
+		if (num < 2) {
+			return 0;
+		}
+		long i;
+		for (i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i == 0)
+				return i - 1;
+
+		}
+		return -i + 1;
+	}
+
+	public static void main(String[] atgs) {
+		Scanner input = new Scanner(System.in);
+		long inputNumber = input.nextLong();
+		long result = isSimple(inputNumber);
+
+		if (result == 0) {
+			System.out.print("Illegal Argument");
+			System.exit(-1);
+		}
+		else if (result < 0) {
+			System.out.printf("true %d", -result);
+		}
+		else {
+			System.out.printf("false %d", result);
+		}
 	}
 }
